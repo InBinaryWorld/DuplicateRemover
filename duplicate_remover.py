b12186652cf534d47673d45d9cbeebead35aebce
Krzysztof Szafraniak <k.szafraniak1@wp.pl>
2023-01-25 13:15:37 +0100

    Optimization

--- duplicate_remover.py
+++ duplicate_remover.py
@@ -82,7 +82,7 @@ def get_bool_from_input():
             return True
         if response.lower() in ["n", "no"]:
             return False
-        print("Invalid response,please choose one of (y/n): ", end='')
+        print("Invalid response, please choose one of (y/n): ", end='')
 
 
 def get_dir_from_input():
@@ -126,13 +126,11 @@ def print_cleaner_stats(initial_count, initial_size, final_count, final_size):
 
 def print_sync_stats(
         count_total, size_total,
-        count_skipped, size_skipped,
         count_removed, size_removed,
         count_copied, size_copied
 ):
     print("\t\tFiles \tSize")
     print("Total: \t\t{} \t{}".format(count_total, to_readable_size(size_total)))
-    print("Skipped: \t{} \t{}".format(count_skipped, to_readable_size(size_skipped)))
     print("Slave Removed: \t{} \t{}".format(count_removed, to_readable_size(size_removed)))
     print("Slave Copied: \t{} \t{}".format(count_copied, to_readable_size(size_copied)))
 
@@ -223,12 +221,11 @@ def move_file(src_root, src_name, dst_root, dst_name):
     shutil.move(src, dst)
 
 
-def get_duplicates_from_array(file_path, path_array):
-    duplicates = []
-    for arr_file_path in path_array:
+def has_duplicates_in_array(file_path, array_base_path, rel_path_array):
+    for arr_file_path in map(lambda p: os.path.join(array_base_path, p), rel_path_array):
         if filecmp.cmp(file_path, arr_file_path, shallow=False):
-            duplicates.append(arr_file_path)
-    return duplicates
+            return True
+    return False
 
 
 def get_hash_of_file(file_path):
@@ -271,77 +268,101 @@ def analyze_paths(paths):
 
 ############################# CODE #############################
 
-@timeit
-def extract_collection_from_dir(root_dir, initCollection={}):
-    def do_nothing(*args):
-        pass
-
-    return collect_files_with_callbacks(root_dir, do_nothing, do_nothing, True, True, initCollection)
-
-
-def remove_duplicated_files(root_dir, initCollection={}):
-    def do_nothing(*args):
-        pass
-
-    def remove(file_path, files_with_same_content):
-        logger.info("Removing duplicated file: " + os.path.relpath(file_path, root_dir))
-        remove_file(file_path)
-
-    return collect_files_with_callbacks(root_dir, remove, do_nothing, False, True, initCollection)
+def for_files(root_dir, debug_msg, callback):
+    for root, _, files in os.walk(root_dir):
+        logger.debug(debug_msg + root)
+        for file_name in files:
+            file_path = os.path.join(root, file_name)
+            rel_path = os.path.relpath(file_path, root_dir)
+            file_hash = get_hash_of_file(file_path)
+            callback(file_path, rel_path, file_hash)
 
 
-def find_existing_and_extra_files(base_dir, work_dir):
-    base_dir_existing = set()
-    work_dir_extra_files = set()
-    work_dir_existing_in_other_path = set()
+def collect_files_by_hash(root_dir):
+    collection = {}
 
-    def on_new(file_path):
-        work_dir_extra_files.add(file_path)
-
-    def on_duplicate(file_path, files_with_same_content):
-        base_dir_path = os.path.join(base_dir, os.path.relpath(file_path, work_dir))
-        if base_dir_path in files_with_same_content:
-            base_dir_existing.add(base_dir_path)
+    def callback(file_path, rel_path, file_hash):
+        if file_hash in collection:
+            collection[file_hash].append(rel_path)
         else:
-            work_dir_existing_in_other_path.add(file_path)
+            collection[file_hash] = [rel_path]
 
+    for_files(root_dir, "Collecting files from: ", callback)
+    return collection
+
+
+def collect_files_by_rel_path(root_dir):
+    collection = {}
+
+    def callback(file_path, rel_path, file_hash):
+        collection[rel_path] = file_hash
+
+    for_files(root_dir, "Collecting files from: ", callback)
+    return collection
+
+
+def remove_duplicated_files(root_dir):
+    collection = {}
+
+    def callback(file_path, rel_path, file_hash):
+        if file_hash in collection:
+            if has_duplicates_in_array(file_path, root_dir, collection[file_hash]):
+                logger.info("Removing duplicated file: " + os.path.relpath(file_path, root_dir))
+                remove_file(file_path)
+            else:
+                collection[file_hash].append(rel_path)
+        else:
+            collection[file_hash] = [rel_path]
+
+    for_files(root_dir, "Removing duplicated files from: ", callback)
+
+
+def remove_duplicated_files_with_source_dir(source_dir, cleaning_dir):
+    collection = collect_files_by_hash(source_dir)
+
+    def callback(file_path, rel_path, file_hash):
+        if file_hash in collection and has_duplicates_in_array(file_path, source_dir, collection[file_hash]):
+            logger.info("Removing duplicated file: " + os.path.relpath(file_path, cleaning_dir))
+            remove_file(file_path)
+
+    for_files(cleaning_dir, "Removing duplicated files from: ", callback)
+
+
+def find_existing_and_extra_files_by_path(base_dir, work_dir, with_deep_compare=False):
     logger.info("Reading Base dir - Started")
-    base_dir_collection = extract_collection_from_dir(base_dir)
+    base_dir_collection = collect_files_by_rel_path(base_dir)
     logger.info("Reading Base dir - Finished")
 
     logger.info("Reading Work dir - Started")
-    collect_files_with_callbacks(work_dir, on_duplicate, on_new, False, False, base_dir_collection)
+    work_dir_collection = collect_files_by_rel_path(work_dir)
     logger.info("Reading Work dir - Finished")
 
-    return base_dir_collection, base_dir_existing, work_dir_extra_files, work_dir_existing_in_other_path
+    logger.info("Checking differences - Started")
+    base_rel_paths = set(base_dir_collection.keys())
+    work_rel_paths = set(work_dir_collection.keys())
 
+    new_files = base_rel_paths.difference(work_rel_paths)
+    extra_files = work_rel_paths.difference(base_rel_paths)
+    existing_files = set()
 
-def collect_files_with_callbacks(root_dir,
-                                 on_duplicate_callback,
-                                 on_new_callback,
-                                 collectOnDuplicate,
-                                 collectOnNew,
-                                 collection={}):
-    for root, _, files in os.walk(root_dir):
-        logger.debug("Processing dir: " + root)
-        for file_name in files:
-            file_path = os.path.join(root, file_name)
-            file_hash = get_hash_of_file(file_path)
-            if file_hash in collection:
-                files_with_same_content = get_duplicates_from_array(file_path, collection[file_hash])
-                if len(files_with_same_content) > 0:
-                    on_duplicate_callback(file_path, files_with_same_content)
-                    if collectOnDuplicate:
-                        collection[file_hash].append(file_path)
-                else:
-                    on_new_callback(file_path)
-                    if collectOnNew:
-                        collection[file_hash].append(file_path)
+    for path in work_rel_paths.intersection(base_rel_paths):
+        if with_deep_compare:
+            base_file_path = os.path.join(base_dir, path)
+            work_file_path = os.path.join(work_dir, path)
+            if filecmp.cmp(base_file_path, work_file_path, shallow=False):
+                existing_files.add(path)
             else:
-                on_new_callback(file_path)
-                if collectOnNew:
-                    collection[file_hash] = [file_path]
-    return collection
+                new_files.add(path)
+                extra_files.add(path)
+        elif base_dir_collection[path] == work_dir_collection[path]:
+            existing_files.add(path)
+        else:
+            new_files.add(path)
+            extra_files.add(path)
+
+    logger.info("Checking differences - Finished")
+
+    return existing_files, new_files, extra_files
 
 
 def rename_files(root_dir, prefix, all_unique):
@@ -376,38 +397,35 @@ def move_files_to_root(root_dir, prefix):
     shutil.rmtree(temp_dir)
 
 
-def synchronize_data(master_dir, slave_dir):
-    m_collection, m_existing, s_extra, s_existing_in_other_path = find_existing_and_extra_files(master_dir, slave_dir)
+def synchronize_data(master_dir, slave_dir, deep_compare):
+    existing_files, new_files, extra_files = find_existing_and_extra_files_by_path(master_dir, slave_dir, deep_compare)
 
-    to_be_skipped = m_existing
-    to_be_removed = s_extra.union(s_existing_in_other_path)
+    to_be_removed = list(map(lambda p: os.path.join(slave_dir, p), extra_files))
+    to_be_copied = list(map(lambda p: os.path.join(master_dir, p), new_files))
 
+    count_total, size_total = analyze_dir(master_dir)
+    count_copied, size_copied = analyze_paths(to_be_copied)
     count_removed, size_removed = analyze_paths(to_be_removed)
-    for path in to_be_removed:
-        logger.info("Removing extra file: " + os.path.relpath(path, slave_dir))
+
+    logger.info("Removing extra files - Started")
+    for path, rel_path in zip(to_be_removed, extra_files):
+        logger.info("Removing extra file: " + rel_path)
         remove_file(path)
+    logger.info("Removing extra files - Finished")
 
     logger.info("Removing empty directories - Started")
     remove_empty_folders(slave_dir)
     logger.info("Removing empty directories - Finished")
 
-    logger.info("Copying files - Started")
-    master_files = {path for paths in m_collection.values() for path in paths}
-    to_be_copied = master_files.difference(to_be_skipped)
-    for master_file in to_be_copied:
-        rel_path = os.path.relpath(master_file, master_dir)
+    logger.info("Copying new files - Started")
+    for master_path, rel_path in zip(to_be_copied, new_files):
         logger.info("Copying file: " + rel_path)
-        slave_file = os.path.join(slave_dir, rel_path)
-        deep_file_copy(master_file, slave_file)
-    logger.info("Copying files - Finished")
-
-    count_total, size_total = analyze_dir(master_dir)
-    count_skipped, size_skipped = analyze_paths(to_be_skipped)
-    count_copied, size_copied = analyze_paths(to_be_copied)
+        slave_path = os.path.join(slave_dir, rel_path)
+        deep_file_copy(master_path, slave_path)
+    logger.info("Copying new files - Finished")
 
     print_sync_stats(
         count_total, size_total,
-        count_skipped, size_skipped,
         count_removed, size_removed,
         count_copied, size_copied
     )
@@ -465,10 +483,10 @@ def menu_remove_with_source_dir():
     analyse_dir_path = get_dir_from_input()
     print("Provide DIRECTORY TO BE CLEANED: \t", end='')
     removing_dir_path = get_dir_from_input()
-    collection = extract_collection_from_dir(analyse_dir_path)
 
     c1, s1 = analyze_dir(removing_dir_path)
-    remove_duplicated_files(removing_dir_path, collection)
+
+    remove_duplicated_files_with_source_dir(analyse_dir_path, removing_dir_path)
 
     print("Info: If you move files to root, files will be renamed")
     print("Do you want to move files to root directory? (y/n): ", end='')
@@ -524,7 +542,10 @@ def menu_sync_data():
     master_dir = get_dir_from_input()
     print("Provide slave dir: ", end='')
     slave_dir = get_dir_from_input()
-    synchronize_data(master_dir, slave_dir)
+    print("Do you want to use deep file comparison? If no hash and size comparison will be used: ", end='')
+    deep_compare = get_bool_from_input()
+
+    synchronize_data(master_dir, slave_dir, deep_compare)
 
 
 def menu_remove_files_with_same_name():
